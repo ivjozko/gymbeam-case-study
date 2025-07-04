52a15c488df21f44c427b6001bdd7ab777f3b0ed
Jozef <62766706+ivjozko@users.noreply.github.com>
2025-07-04 14:15:03 +0200

    Update sql_transformation.sql
    uprava komentov

--- 03_manual_input/sql_transformation.sql
+++ 03_manual_input/sql_transformation.sql
@@ -1,13 +1,13 @@
 -- Vytvorenie očistenej tabuľky s validovanými a skontrolovanými dátami
 CREATE OR REPLACE TABLE clean_csv_input AS
 SELECT
-		-- Zachovávame pôvodné stĺpce, číselné hodnoty konvertujeme do správneho typu
+-- Zachovávame pôvodné stĺpce, číselné hodnoty konvertujeme do správneho typu
     TransactionID,
     Category,
     Product,
     TransactionDate,
-    SAFE_CAST(Quantity AS INT64) as Quantity,					-- Prevádza Quantity na celé číslo
-    SAFE_CAST(Price AS FLOAT64) as Price,							-- Prevádza Price na desatinné číslo
+    SAFE_CAST(Quantity AS INT64) as Quantity,			-- Prevádza Quantity na celé číslo
+    SAFE_CAST(Price AS FLOAT64) as Price,			-- Prevádza Price na desatinné číslo
     SAFE_CAST(TotalValue AS FLOAT64) as TotalValue,		-- Prevádza TotalValue na desatinné číslo
     CustomerID,
     PaymentMethod,
@@ -15,11 +15,11 @@ SELECT
     Email,
     OrderStatus,
     DiscountCode,
-    SAFE_CAST(PaymentAmount AS FLOAT64) as PaymentAmount,		-- Prevádza PaymentAmount na desatinné číslo
-    _timestamp AS import_timestamp													-- Premenujeme systémový stĺpec, aby bol validný v BigQuery
+    SAFE_CAST(PaymentAmount AS FLOAT64) as PaymentAmount,	-- Prevádza PaymentAmount na desatinné číslo
+    _timestamp AS import_timestamp				-- Premenujeme systémový stĺpec, aby bol validný v BigQuery
 FROM csv_input
 WHERE
-		-- Kontrola povinných textových polí (nesmie byť NULL ani prázdny string)
+-- Kontrola povinných textových polí (nesmie byť NULL ani prázdny string)
     Category IS NOT NULL AND Category != '' 
     AND Product IS NOT NULL AND Product != ''
     AND TransactionDate IS NOT NULL
